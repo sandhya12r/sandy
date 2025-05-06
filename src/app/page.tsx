20b65d2b70ccfce5df80c538d3348b59e1bfb29f
sandhya12r <126336441+sandhya12r@users.noreply.github.com>
2025-05-06 23:24:17 +0530

    add promptbox

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { MenuIcon, MessageCircle } from "lucide-react";
 import Image from "next/image";
 import { useState } from "react";
 import Sidebar from "@/components/Sidebar";
+import PromptBox from "@/components/PromptBox";
 export default function Home() {
   const [expand, setExpand] = useState(false);
   const [messages, setMessages] = useState<string[]>([]);
@@ -29,7 +30,7 @@ export default function Home() {
               <div></div>
             )}
 
-          {/* prompt box */}
+          <PromptBox isLoading={isLoading} setIsLoading={setIsLoading}/>
           <p className="text-xs absolute bottom-2 text-gray-300">Sandy can make mistakes, for reference only.</p>
         </div>
       </div>
--- src/components/PromptBox.tsx
+++ src/components/PromptBox.tsx
@@ -1,7 +1,11 @@
-import { Pin, Search, Sparkle } from 'lucide-react'
+import { Globe, Paperclip, Sparkle, Upload } from 'lucide-react'
 import React, { useState } from 'react'
 
-const PromptBox = () => {
+interface PromptBoxprops {
+    isLoading: Boolean;
+    setIsLoading: React.Dispatch<React.SetStateAction<boolean>>
+}
+const PromptBox: React.FC<PromptBoxprops> = ({ isLoading, setIsLoading }) => {
     const [prompt, setPrompt] = useState('');
     return (
         <form className={`w-full ${false ? "max-w-3xl" : "max-w-2xl"} bg-[#404045] p-4 rounded-3xl mt-4 transition-all`}>
@@ -9,23 +13,23 @@ const PromptBox = () => {
                 className='outline-none w-full resize-none overflow-hidden break-words bg-transparent'
                 rows={2}
                 placeholder='Message Sandy' required
-                onChange={(e)=> setPrompt(e.target.value)}
+                onChange={(e) => setPrompt(e.target.value)} value={prompt}
             />
             <div className='flex items-center justify-between text-sm'>
                 <div className='flex items-center gap-2'>
-                    <p className=' flex items-center gap-2 text-sm border border-gray-300/40 px-2 py-1 rounded-full cursor-pointer hover:bg-gray-500/20 transition'>
-                        <span className='h-5'><Sparkle /></span>
+                    <p className='flex items-center gap-2 text-sm border border-gray-300/40 px-2 py-1 rounded-full cursor-pointer hover:bg-gray-500/20 transition'>
+                        <span className='w-5'><Sparkle className='w-5' /></span>
                         Sandy AI
                     </p>
-                    <p className=' flex items-center gap-2 text-sm border border-gray-300/40 px-2 py-1 rounded-full cursor-pointer hover:bg-gray-500/20 transition'>
-                        <span className='h-5'><Search /></span>
+                    <p className='flex items-center gap-2 text-sm border border-gray-300/40 px-2 py-1 rounded-full cursor-pointer hover:bg-gray-500/20 transition'>
+                        <span className='w-5'><Globe className='w-5' /></span>
                         Search
                     </p>
                 </div>
-                <div className='flex items-center gap-2'>
-                    <span className='w-4 cursor-pointer'><Pin /></span>
-                    <button>
-                        <span className='w-4 cursor-pointer'><Pin /></span>
+                <div className='flex items-center gap-3'>
+                    <span className='w-4 cursor-pointer'><Paperclip /></span>
+                    <button className={`${prompt ? "bg-amber-100" : "bg-[#71717a]"} rounded-full p-1 cursor-pointer`}>
+                        <span className='w-3.5 aspect-square'><Upload /></span>
                     </button>
                 </div>
             </div>
